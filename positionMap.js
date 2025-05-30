de85fbfe057b39573ca176ab17eac75276937e4e
Aakash9665 <aakashmishra510@gmail.com>
2025-05-30 12:16:16 +0530

    Create positionMap.js

---
+++ positionMap.js
@@ -0,0 +1,11 @@
+const positionMap = {
+  TecoreLabs: ["Full Stack Developer", "PostgreSQL Database Administrator"],
+  Saviesahome: ["Interior Designer"],
+  angelportal: [
+    "Hr Recruiter",
+    "Video Editor and Graphic Designer",
+    "Relationship Manager",
+    "Python Developer",
+    "SEO and Social Media Manager"
+  ]
+};
